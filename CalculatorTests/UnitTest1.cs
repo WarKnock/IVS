0129d73e34e8f87113fbb76b64260cba0495baea
Tomáš Kurtin <xkurti03@stud.fit.vutbr.cz>
2021-04-05 20:53:10 +0200

    Added basic structure to Tests

--- CalculatorTests/UnitTest1.cs
+++ CalculatorTests/UnitTest1.cs
@@ -1,13 +1,76 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
+using turbocalc;
+
 namespace CalculatorTests
 {
     [TestClass]
-    public class UnitTest1
+    public class Add
     {
         [TestMethod]
-        public void TestMethod1()
+        public void BasicTest()
         {
+            
+        }
+    }
+
+    [TestClass]
+    public class Subtract
+    {
+        [TestMethod]
+        public void BasicTest()
+        {
+
+        }
+    }
+
+    [TestClass]
+    public class Multiply
+    {
+        [TestMethod]
+        public void BasicTest()
+        {
+
+        }
+    }
+
+    [TestClass]
+    public class Divide
+    {
+        [TestMethod]
+        public void BasicTest()
+        {
+
+        }
+    }
+
+    [TestClass]
+    public class Power
+    {
+        [TestMethod]
+        public void BasicTest()
+        {
+
+        }
+    }
+
+    [TestClass]
+    public class Root
+    {
+        [TestMethod]
+        public void BasicTest()
+        {
+
+        }
+    }
+
+    [TestClass]
+    public class Factorial
+    {
+        [TestMethod]
+        public void BasicTest()
+        {
+
         }
     }
 }
