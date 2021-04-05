96fd30eb23723aa178141f8b1eb28e2a5da379c7
Tomáš Kurtin <xkurti03@stud.fit.vutbr.cz>
2021-04-05 20:26:43 +0200

    Basic structure of calculator class

--- turbocalc/Calculator.cs
+++ turbocalc/Calculator.cs
@@ -2,6 +2,88 @@
 {
     public class Calculator
     {
-        
+        /**
+         * @brief Adds two numbers a+b
+         * 
+         * @param a First number
+         * @param b Second number
+         * @return double
+         */
+        public static double Add(double a, double b)
+        {
+            return 0;
+        }
+
+        /**
+         * @brief Subtracts two numbers a-b
+         * 
+         * @param a First number
+         * @param b Second number
+         * @return double
+         */
+        public static double Subtract(double a, double b)
+        {
+            return 0;
+        }
+
+        /**
+         * @brief Divides two numbers a/b
+         * 
+         * @param a First number
+         * @param b Second number
+         * @return double
+         */
+        public static double Divide(double a, double b)
+        {
+            return 0;
+        }
+
+        /**
+         * @brief Multiplies two numbers a*b
+         * 
+         * @param a First number
+         * @param b Second number
+         * @return double
+         */
+        public static double Mupltiply(double a, double b)
+        {
+            return 0;
+        }
+
+        /**
+         * @brief Makes Power x^n
+         * 
+         * @param x Number to make power of
+         * @param n exponent
+         * @return double
+         */
+        public static double Power(double x, int n)
+        {
+            return 0;
+        }
+
+        /**
+         * @brief Makes n-th root of number x - x^(1/n)
+         * 
+         * @param x Number to make square of
+         * @param n n-th root
+         * @return double
+         */
+        public static double Root(double x, int n)
+        {
+            return 0;
+        }
+
+
+        /**
+         * @brief Makes factorial of x - x!
+         * 
+         * @param x Number to make factorial of
+         * @return int
+         */
+        public static int Factorial(int x)
+        {
+            return 0;
+        }
     }
 }
