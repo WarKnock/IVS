04eb2f1b5c49a7548aa92a8b8483a0783793a6ab
Tomáš Kurtin <xkurti03@stud.fit.vutbr.cz>
2021-04-05 21:07:16 +0200

    Test for Add

--- CalculatorTests/UnitTest1.cs
+++ CalculatorTests/UnitTest1.cs
@@ -7,11 +7,30 @@ namespace CalculatorTests
     [TestClass]
     public class Add
     {
+        /**
+         * @brief Testing adding two integers
+         */
         [TestMethod]
         public void BasicTest()
         {
-            
+            Assert.AreEqual(3, Calculator.Add(1,2));
+            Assert.AreNotEqual(0, Calculator.Add(1,2));
+            Assert.AreEqual(-5, Calculator.Add(1,-6));
+            Assert.AreEqual(-3, Calculator.Add(-1,-2));
         }
+
+        /**
+         * @brief Testing adding two double numbers
+         */
+        [TestMethod]
+        public void DoubleTest()
+        {
+            Assert.AreEqual(3.5, Calculator.Add(1.2, 2.3));
+            Assert.AreNotEqual(0, Calculator.Add(1.7, 2.8));
+            Assert.AreEqual(-5, Calculator.Add(1.4, -6.4));
+            Assert.AreEqual(-3, Calculator.Add(-1.2, -1.8));
+        }
+
     }
 
     [TestClass]
