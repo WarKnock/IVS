6c595ebb60bcfd064396992d36b62458db91fa8a
Tomáš Kurtin <xkurti03@stud.fit.vutbr.cz>
2021-04-06 15:55:53 +0200

    Rewritten documentation to suit doxygen style

--- turbocalc/Calculator.cs
+++ turbocalc/Calculator.cs
@@ -1,86 +1,87 @@
 ﻿namespace turbocalc
 {
+    /// <summary>
+    /// Math class for calculator
+    /// </summary>
     public class Calculator
     {
-        /**
-         * @brief Adds two numbers a+b
-         * 
-         * @param a First number
-         * @param b Second number
-         * @return double
-         */
+        /// <summary>
+        /// Adds two numbers - a+b 
+        /// </summary>
+        /// <param name="a">First number</param>
+        /// <param name="b">Second number</param>
+        /// <returns>double a+b</returns>
         public static double Add(double a, double b)
         {
             return 0;
         }
 
-        /**
-         * @brief Subtracts two numbers a-b
-         * 
-         * @param a First number
-         * @param b Second number
-         * @return double
-         */
+        /// <summary>
+        /// Subtracts two numbers - a-b
+        /// </summary>
+        /// <param name="a">First number</param>
+        /// <param name="b">Second number</param>
+        /// <returns>double a-b</returns>
         public static double Subtract(double a, double b)
         {
             return 0;
         }
 
-        /**
-         * @brief Divides two numbers a/b
-         * 
-         * @param a First number
-         * @param b Second number
-         * @return double
-         */
+        /// <summary>
+        /// Divides two numbers - a/b
+        /// </summary>
+        /// <param name="a">First number</param>
+        /// <param name="b">Second number</param>
+        /// <returns>double a/b</returns>
         public static double Divide(double a, double b)
         {
             return 0;
         }
 
-        /**
-         * @brief Multiplies two numbers a*b
-         * 
-         * @param a First number
-         * @param b Second number
-         * @return double
-         */
+        /// <summary>
+        /// Multiplies two numbers - a*b
+        /// </summary>
+        /// <param name="a">First number</param>
+        /// <param name="b">Second number</param>
+        /// <returns>double a*b</returns>
         public static double Mupltiply(double a, double b)
         {
             return 0;
         }
 
-        /**
-         * @brief Makes Power x^n
-         * 
-         * @param x Number to make power of
-         * @param n exponent
-         * @return double
-         */
+        /// <summary>
+        /// Makes n-th power of x - x^n
+        /// </summary>
+        /// <param name="x">Number to make power of</param>
+        /// <param name="n">Exponent of power</param>
+        /// <returns>double x^n</returns>
         public static double Power(double x, int n)
         {
             return -1;
         }
 
-        /**
-         * @brief Makes n-th root of number x - x^(1/n)
-         * 
-         * @param x Number to make square of
-         * @param n n-th root
-         * @return double returns -1 if x < 0 else returns root
-         */
+        /// <summary>
+        /// Makes n-th root of x - x^(1/n)
+        /// </summary>
+        /// <param name="x">Number to make root of</param>
+        /// <param name="n">Exponent of root</param>
+        /// <returns>  
+        ///     double root x^(1/n),
+        ///     if x is lesser than 0 then it returns -1
+        /// </returns>
         public static double Root(double x, int n)
         {
             return -2;
         }
 
 
-        /**
-         * @brief Makes factorial of x - x!
-         * 
-         * @param x Number to make factorial of
-         * @return int returns -1 if x < 0, returns 1 if x == 0 else returns factorial
-         */
+        /// <summary>
+        /// Makes factorial of a number x - x!
+        /// </summary>
+        /// <param name="x">Number to make factorial of</param>
+        /// <returns>
+        ///     int x!; if x==0 then returns 1; if x is lesser than 0 than returns -1
+        /// </returns>
         public static int Factorial(int x)
         {
             return 0;
